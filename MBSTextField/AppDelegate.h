8418c0b4f135bcda70b93d60df7757683238ab06
your name <your@email.com>
2018-07-24 16:04:15 +0800

    Initial commit

---
+++ MBSTextField/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MBSTextField
+//
+//  Created by chc on 2018/7/24.
+//  Copyright © 2018年 chc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MBSTextField/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MBSTextField
+//
+//  Created by chc on 2018/7/24.
+//  Copyright © 2018年 chc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
